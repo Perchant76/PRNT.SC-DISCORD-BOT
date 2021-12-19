f025dc016ea7b9560ce3d4031a538af100004f24
Perchant76 <63096336+Perchant76@users.noreply.github.com>
2021-12-19 14:08:21 +0100

    Update

--- prnt.sc.py
+++ prnt.sc.py
@@ -4,7 +4,7 @@ import time
 from discord import channel
 
 # there you can insert your discord token
-token = ("INSERT YOUR TOKEN HERE")
+token = ("OTE5MzUxMjQ4OTkyNTAxNzgw.YbUikg.wAD9a5PXq4PcAXoh1SK8tGInawk")
 # this is the prnt.sc url that we use to create full link
 prnt_sc = ("https://prnt.sc/")
 # protocol
@@ -27,7 +27,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("We are allready logged in as {0.user}".format(client))
+    print("We are allready logged in as: {0.user}".format(client))
 
 
 def mejn():
@@ -35,48 +35,47 @@ def mejn():
     async def on_message(message):
         if message.author == client.user:
             return
-        if message.content.startwith("&help"):
-            await message.channel.send("""` &help ` - show help
-` &prntsc ` - show 1 random print screen
-` &prntsc10 ` - show 10 random printscreens
-` &prntsc50 ` - show 50 random print screens
-` &prntsc100 ` - show 100 random print screens
-` &prntsc500 ` - show 500 random print screens
-` &prntsc10000 ` - show 10 000 random print screens
-|| Bot CREATED BY Perchant#2178 and Ccondir#0556 ||""")
-        if message.content.startwith("&ping"):
-            await message.channel.send("pong")
-        if message.content.startwith("&prntsc1"):
+        if message.content.startswith("&help"):
+            await message.channel.send("""
+    `&help` - show help
+    ```&prntsc1 - shows 1 random print screen
+    &prntsc10 - shows 10 random print screens
+    &prntsc3 - shows 100 random print screens
+    &prntsc4 - shows 1000 random print screens
+    &prntsc5 - shows 10000 random print screens```
+    ||Bot created by Ccondir#0556 and Perchant#2178||
+            """)
+        if message.content.startswith("&prntsc"):
             await message.channel.send("**sendin 1 prntsc**")
             await message.channel.send("https://prnt.sc/"+random.choice(a)+random.choice(b)+random.choice(c)+random.choice(d)+random.choice(e)+random.choice(f))
             await message.channel.send("**done**")
-        if message.content.startwith("&prntsc2"):
+        if message.content.startswith("&prntsc10"):
             await message.channle.send("**sendin 10 prtscs**")
             for i in range(10):
                 await message.channel.send("https://prnt.sc/"+random.choice(a)+random.choice(b)+random.choice(c)+random.choice(d)+random.choice(e)+random.choice(f))
                 time.sleep(1)
             await message.channel.send("**done**")
-        if message.content.startwith("&prntsc3"):
+        if message.content.startswith("&prntsc100"):
             await message.channel.send("**sendin 100 prtscs**")
             for i in range(100):
                 await message.channel.send("https://prnt.sc/"+random.choice(a)+random.choice(b)+random.choice(c)+random.choice(d)+random.choice(e)+random.choice(f))
                 time.sleep(1)
             await message.channel.send("**done**")
-        if message.content.startwith("&prntsc4"):
+        if message.content.startswith("&prntsc1000"):
             await message.channel.send("**sendin 1000 prtscs**")
             for i in range(1000):
                 await message.channel.send("https://prnt.sc/"+random.choice(a)+random.choice(b)+random.choice(c)+random.choice(d)+random.choice(e)+random.choice(f))
                 time.sleep(1)
             await message.channel.send("**done**")
-        if message.content.startwith("&prntsc5"):
+        if message.content.startswith("&prntsc10000"):
             await message.channel.send("**sendin 10000 prtscs**")
             for i in range(10000):
                 await message.channel.send("https://prnt.sc/"+random.choice(a)+random.choice(b)+random.choice(c)+random.choice(d)+random.choice(e)+random.choice(f))
                 time.sleep(1)
             await message.channel.send("**done**")
+    client.run(token)
 
 
-client.run(token)
 for i in range(999999):
     mejn()
     time.sleep(1)
